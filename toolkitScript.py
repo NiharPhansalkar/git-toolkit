ba1058a61ea298d036bdeaf1b5bb14099f9e7ea3
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-06-16 08:48:40 +0000

    added functionality to view commit diffs across various branches

--- toolkitScript.py
+++ toolkitScript.py
@@ -19,18 +19,32 @@ while True:
         print("Please enter a correct repository path")
         repoPath = Path(input("Enter the absolute path to your repository: ")).expanduser()
 
-
 def diffCommits(repoObject):
     totalBranches = int(input("How many branches will you like to add? "))
     branches = []
     commitTable = {}
-    finalValues = {}
     for i in range(totalBranches):
         branchName = input(f"Enter branch{i+1} name: ")
         branches.append(branchName)
 
     for individualBranch in branches:
         branch = repo.commit(individualBranch);
-        commitTable[individualBranch] = list(repo.iter_commits(branch));
+        commitTable[individualBranch] = list(repo.iter_commits(branch))
+
+    uniqueValues = set()
+
+    for lst in commitTable.values():
+        uniqueValues.update(lst)
+
+    columns = {name: [None] * len(uniqueValues) for name in commitTable.keys()}
+
+    df = pd.DataFrame(columns)
+
+    for i, value in enumerate(uniqueValues):
+        for name, lst in commitTable.items():
+            if value in lst:
+                df.at[i, name] = value
+
+    df.to_csv('output.csv', index=False)
 
 diffCommits(repo)
